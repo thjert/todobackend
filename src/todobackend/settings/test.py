f244fd74cfc69f82f43880f6668034785ae0aadb
Thomas Hjert <thomas.hjert@knowit.se>
2018-02-09 14:17:58 +0000

    Successfully ran tests based on this:
    ----------------------------------------------------------------------
    XML: /home/vagrant/todobackend/src/unittest.xml
    Name                              Stmts   Miss  Cover
    -----------------------------------------------------
    todo/__init__.py                      0      0   100%
    todo/admin.py                         1      1     0%
    todo/migrations/0001_initial.py       6      0   100%
    todo/migrations/__init__.py           0      0   100%
    todo/models.py                        7      7     0%
    todo/serializers.py                   7      0   100%
    todo/urls.py                          6      0   100%
    todo/views.py                        18      0   100%
    -----------------------------------------------------
    TOTAL                                45      8    82%
    ----------------------------------------------------------------------
    Ran 12 tests in 0.290s
    
    OK

--- src/todobackend/settings/test.py
+++ src/todobackend/settings/test.py
@@ -1,5 +1,21 @@
 from base import *
 import os
+# Installed Apps
+INSTALLED_APPS += ('django_nose',)
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR','.')
+NOSE_ARGS = [
+  '--verbosity=2',
+  '--nologcapture',
+  '--with-coverage',
+  '--cover-package=todo',
+  '--with-spec',
+  '--spec-color',
+  '--with-xunit',
+  '--xunit-file=%s/unittest.xml' % TEST_OUTPUT_DIR,
+  '--cover-xml',
+  '--cover-xml-file=%s/converage.xml' % TEST_OUTPUT_DIR,
+]
 
 DATABASES = {
     'default': {
