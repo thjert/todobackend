490434e11d0bb2dbfcb9844ea583e7fc56f78685
root <root@acs>
2018-02-16 16:30:37 +0000

    Flyttat över filer från todobackend-no-up, förhoppningsvis blev det rätt.

--- src/setup.py
+++ src/setup.py
@@ -7,7 +7,7 @@ setup (
   packages             = find_packages(),
   include_package_data = True,
   scripts              = ["manage.py"],
-  install_requires     = ["Django>=1.9,<2.0",
+  install_requires     = ["Django>=1.9",
                           "django-cors-headers>=1.1.0",
                           "djangorestframework>=3.3.1",
                           "MySQL-python>=1.2.5",
